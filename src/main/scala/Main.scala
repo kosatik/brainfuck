af24ea1f05e0736b2434f770a00051a5db146387
timofeev <k.timofeev@butik.ru>
2018-06-17 21:54:44 +0300

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,3 @@
-import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 //санаторная , пионерская
@@ -7,13 +6,11 @@ object Main extends App {
   private val buf = new Array[Int](300)
   private var cursor = 0: Int
   private var cursorData = 0: Int
-  private var loopNumber = 0: Int
   private var lastIndex = 0: Int
 
   def run() {
     val PATH = getClass.getResource("").getPath
     val file = Source.fromFile(PATH + "brainfuck.txt")
-
     try {
       var i = 0
       var lastChar = ""
@@ -24,27 +21,18 @@ object Main extends App {
           i += 1
         }
       }
-      //      println(data.lastIndexOf(lastChar))
       lastIndex = data.lastIndexOf(lastChar)
-      eachData(cursorData, loopNumber, lastIndex)
+      eachData(cursorData, lastIndex)
     } finally {
       file.close()
     }
   }
 
-  private def eachData(from: Int, loop: Int, to: Int): Unit = {
+  private def eachData(from: Int, to: Int): Unit = {
     cursorData = from
     for (i <- from until to) {
-//      print(data(i))
-//      println(loop, loopNumber, data(i))
-
-//      if (loopNumber != loop) {
-//        return
-//      }
-//      if (loopNumber == loop) {
         parse(data(i))
         cursorData += 1
-//      }
     }
 
 
@@ -55,29 +43,25 @@ object Main extends App {
     else if (char.equals("-")) buf(cursor) -= 1
     else if (char.equals(">")) cursor += 1
     else if (char.equals("<")) cursor -= 1
-    else if (char.equals(".")) println(buf(cursor).toChar)
+    else if (char.equals(".")) print(buf(cursor).toChar)
     else if (char.equals("[")) {
-//      loopNumber += 1
-      each(cursorData, cursor, loopNumber)
+      each(cursorData, cursor)
     }
     else if (char.equals("]")) {
-//      loopNumber -= 1
     }
   }
 
-  private def each(from: Int, buferCursor: Int, loop: Int): Unit = {
+  private def each(from: Int, buferCursor: Int): Unit = {
     val count = buf(buferCursor)
     val to = findTo(from)
     for (i <- 1 until count) {
-//      println("iter: " + i)
-      eachData(from + 1, loop, to)
+      eachData(from + 1, to)
     }
     cursor = buferCursor
   }
 
   private def findTo(from: Int): Int = {
     for (i <- from until lastIndex) {
-//      println(data(i))
       if (data(i).equals("]")) {
         return i
       }
